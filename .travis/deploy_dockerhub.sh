70fac2cd12a6ced8c18cb67cad485ddb7b20ac58
sergey1701 <sergeyblinov348@gmail.com>
2022-04-02 19:20:29 +0000

    Docker Hub Test4

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "main" ]; then
     TAG="latest"
 else
